b0aca5ba3ee6aa134ca90da03ee0a8adc0d58311
Okechukwu Ndubisi <okechukwundubisi@globusbank.com>
2022-04-20 13:05:57 +0100

    still working on tablet responsiveness for tablet

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,10 +6,11 @@ import { Component,  OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  mq: MediaQueryList | undefined;
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
   openAndClose(event: Event){
@@ -17,8 +18,15 @@ export class SidebarComponent implements OnInit {
     if(li){
       const ulSiblingElement = li.nextElementSibling;
       ulSiblingElement?.classList.toggle('elongate');
-    } 
-    
+    }   
+  }
+
+  closeOnClickIfOnTablets(event: Event){
+    this.mq = window.matchMedia('(min-width: 640px) and (max-width: 1024px)');
+    if(this.mq.matches){
+      const ul = (event.target as HTMLElement).closest('.elongate');
+      ul?.classList.toggle('elongate');
+    }
   }
 
   toggleMobileMenuSideBar(){
